4629cd94e665291033e7bc5f819c10f3e30361b7
Aiko Vandeputte <aiko@aikovdp.me>
2024-05-09 14:53:43 +0200

    Manually declare JUnit platform launcher Fixes Gradle deprecation warnings related to https://docs.gradle.org/current/userguide/upgrading_version_8.html#test_framework_implementation_dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,8 @@ dependencies {
 
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
     testImplementation("ch.qos.logback:logback-classic:1.5.6")
 }
 

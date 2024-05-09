8451a9570a06c11dfa1b19381cf0e788a91a0261
Aiko Vandeputte <aiko@aikovdp.me>
2024-05-09 14:35:55 +0200

    Rename GetGitHubUserActionTest

--- src/test/java/me/aikovdp/jormungandr/GetGitHubUserActionTest.java
+++ src/test/java/me/aikovdp/jormungandr/GetGitHubUserActionTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class GitHubActionTest {
+public class GetGitHubUserActionTest {
     @Test
     public void testGitHubUserAction() {
         GetGitHubUserAction action = new GetGitHubUserAction();
